d4f0b557f29f6e98a9a65a9e9a0eb52da5b78d68
Xavier Villaneau <xvillaneau@gmail.com>
2015-07-04 13:32:04 +0200

    Reset button now also resets links

--- static/opa-menu-left.js
+++ static/opa-menu-left.js
@@ -104,6 +104,8 @@ function resetDisplay() {
 	app.graph.selectAll(".node")
 		.style("fill", function(d) { return app.maincolor(d.type_id); })
 		.style('fill-opacity','1').style('stroke-opacity','1');
+	app.graph.selectAll(".link")
+		.style('stroke-opacity','1');
 };
 
 function initLeftMenu() {
